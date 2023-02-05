72d2014e26f20e19a8733b3ef710de3afa357232
Abishek H <abixshek@gmail.com>
2023-02-05 15:33:18 +0530

    mc

--- src/App.jsx
+++ src/App.jsx
@@ -17,43 +17,41 @@ const App = () => {
   const [outputvalue, setoutputvalue] = useState();
   const output = () => {
     //celsius
-    if (selectedinvalue == "Celsius" && selectedoutvalue == "Fahrenheit") {
-      setoutputvalue(((Number(inputvalue) * 9) / 5 + 32).toFixed(2));
-    } else if (selectedinvalue == "Celsius" && selectedoutvalue == "Celsius") {
-      setoutputvalue(inputvalue);
-    } else if (selectedinvalue == "Celsius" && selectedoutvalue == "Kelvin") {
-      setoutputvalue((Number(inputvalue) + 273.15).toFixed(2));
+    if (selectedinvalue == "Celsius") {
+      if (selectedoutvalue == "Fahrenheit") {
+        setoutputvalue(((Number(inputvalue) * 9) / 5 + 32).toFixed(2));
+      } else if (selectedoutvalue == "Celsius") {
+        setoutputvalue(inputvalue);
+      } else if (selectedoutvalue == "Kelvin") {
+        setoutputvalue((Number(inputvalue) + 273.15).toFixed(2));
+      }
     }
     // //Fahrenheit
-    else if (
-      selectedinvalue == "Fahrenheit" &&
-      selectedoutvalue == "Fahrenheit"
-    ) {
-      setoutputvalue(inputvalue);
-    } else if (
-      selectedinvalue == "Fahrenheit" &&
-      selectedoutvalue == "Celsius"
-    ) {
-      setoutputvalue(((Number(inputvalue) - 32) * (5 / 9)).toFixed(3));
-    } else if (
-      selectedinvalue == "Fahrenheit" &&
-      selectedoutvalue == "Kelvin"
-    ) {
-      setoutputvalue(((inputvalue - 32) * (5 / 9) + 273.15).toFixed(3));
+    else if (selectedinvalue == "Fahrenheit") {
+      if (selectedoutvalue == "Fahrenheit") {
+        setoutputvalue(inputvalue);
+      } else if (selectedoutvalue == "Celsius") {
+        setoutputvalue(((Number(inputvalue) - 32) * (5 / 9)).toFixed(3));
+      } else if (selectedoutvalue == "Kelvin") {
+        setoutputvalue(((inputvalue - 32) * (5 / 9) + 273.15).toFixed(3));
+      }
     }
     // //kelvin
-    else if (selectedinvalue == "Kelvin" && selectedoutvalue == "Fahrenheit") {
-      setoutputvalue(((inputvalue - 273.15) * (9 / 5) + 32).toFixed(3));
-    } else if (selectedinvalue == "Kelvin" && selectedoutvalue == "Celsius") {
-      setoutputvalue((inputvalue - 273.15).toFixed(3));
-    } else {
-      setoutputvalue(inputvalue);
+    else if (selectedinvalue == "Kelvin") {
+      if (selectedoutvalue == "Fahrenheit") {
+        setoutputvalue(((inputvalue - 273.15) * (9 / 5) + 32).toFixed(3));
+      } else if (selectedoutvalue == "Celsius") {
+        setoutputvalue((inputvalue - 273.15).toFixed(3));
+      } else if (selectedoutvalue == "Kelvin") {
+        setoutputvalue(inputvalue);
+      }
     }
   };
   const refresh = () => {
     setinputvalue("");
     setoutputvalue("");
   };
+
   return (
     <div className="App">
       <div className="king-container">
